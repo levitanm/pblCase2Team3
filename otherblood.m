65dcfa503fd4a48c1ed1d26158e6fee3d57690c3
LiamMerv <52580473+LiamMerv@users.noreply.github.com>
2019-11-16 22:11:20 -0600

    Add files via upload

--- otherblood.m
+++ otherblood.m
@@ -1,53 +1,11 @@
-function [t, vO2j, vCO2j, vCaj, vFej, vHCO3j, vGlucosej, vNaj, vErythrocytesj] = otherblood(t, vO2i, vCO2i, vCai, vGlucosei, vFei, vHCO3i, vNai, vErythrocytesi)
-%for all of the components, adjustments should be made based on what is
-%being inputted to other blood and what the output values should be to the
-%other organs. For example, when t = 2, blood is entering from the heart
-%and then this blood is entering the liver so, based on the output value of
-%the components from the heart and the input values needed for the liver,
-%adjustments will be made to each component in other blood. 
-%Another direction we could take is trying to find specific values for adjustments
-%of each component at each time (heart to liver, liver to kidneys, kidneys
-%to heart) but this will take a lot of research to determine. 
-if t == 2
-    %This will be the blood traveling to the liver
-    %vblood = ?, do I just assume it is the vblood from the heart or should
-    %I adjust it based on the volume of blood in 'otherblood'?
-    % CGlucose = x; %this will be the concentration of glucose entering
-    % from the digestive tract, will also depend on how much nutrients the
-    % patient is eating
-    % vGlucosegen = CGlucose * vblood
-    % CCa = y %concentration of calcium entering from bones
-    % vCagen = CCa * vblood
-    vErythrocytesj = vErythrocytesi * 2000000 * 60; %number of RBC entering through the blood marrow per min (though in research it does mention that the same number is being recycled/cleared so this probably needs to be changed)
-    vO2j = vO2i;
-    vCO2j = vCO2i;
-    vCaj = vCai * vCagen;
-    vNaj = vNai;
-    vGlucosej = vGlucosei + VGlucosegen; %VGlucosegen will be addition of glucose from digestive tract, still need to find the value
-    vFej = vFei;
-    vHCO3j = vHCO3i;
-    t = t + 1;
-elseif t == 4
-    %blood is entering from liver and leaving to kidneys
-    vErythrocytesj = vErythrocytesi;
-    vO2j = vO2i;
-    vCO2j = vCO2i;
-    vCaj = vCai;
-    vNaj = vNai;
-    vGlucosej = vGlucosei;
-    vFej = vFei;
-    vHCO3j = vHCO3i;
-    t = t + 1;
-elseif t == 6
-    %blood is entering from kidneys and going to heart
-    vErythrocytesj = vErythrocytesi;
-    vO2j = vO2i;
-    vCO2j = vCO2i;
-    vCaj = vCai;
-    vNaj = vNai;
-    vGlucosej = vGlucosei;
-    vFej = vFei;
-    vHCO3j = vHCO3i;
-    t = t + 1;
-end
+function [Cout] = otherblood(flow, Cin, carbs, calciumintake, sodiumintake, ironintake)
+Cout = [];
+Cout(1) = Cin(1);
+Cout(2) = Cin(2);
+Cout(3) = Cin(3);
+Cout(4) = Cin(4);
+Cout(5) = (flow*Cin(5) + (.8*carbs)/180.156)/flow; %80 percent is directly translated to glucose, 20% is fructose and galactose which goes to the liver and may be converted to glucose at a later stage so another term is incoming
+Cout(6) = (flow*Cin(6) + (sodiumintake/(1000*22.99)))/flow; %500 mg of sodium needed for vital functions so start with sodium intake being 500 mg
+Cout(7) = (flow*Cin(7) + (calciumintake*.26)/(1000*40.08))/flow; %calcium intake should be around 1000 mg
+Cout(8) = (flow*Cin(8) + (ironintake*.18)/(1000*55.845))/flow;%iron intake will be approximately 8 mg for males and 18 mg for females
 end
